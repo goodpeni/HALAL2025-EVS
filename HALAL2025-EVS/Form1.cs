c0d0640fe1154797333109bee0a0da32e0dd9853
weimodein <jhorentuazon.dev@gmail.com>
2025-04-27 01:08:10 +0800

    SQL login for student has been added.

--- HALAL2025-EVS/Form1.cs
+++ HALAL2025-EVS/Form1.cs
@@ -2,19 +2,83 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace HALAL2025_EVS
 {
     public partial class LOGIN : Form
     {
+        string connectionString = "datasource=127.0.0.1;port=3306;username=root;password=;database=evotingdb";
+
         public LOGIN()
         {
             InitializeComponent();
+        }
+
+        public void Login()
+        {
+            //string adminQuery = "SELECT * FROM admin WHERE admin_name = @id AND admin_password = @pass";
+            string studentQuery = "SELECT * FROM student WHERE student_id = @id AND student_password = @pass";
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                try
+                {
+                    connection.Open();
+
+                    using (MySqlCommand studentCmd = new MySqlCommand(studentQuery, connection))
+                    {
+                        studentCmd.Parameters.AddWithValue("@id", TxtStudentID.Text);
+                        studentCmd.Parameters.AddWithValue("@pass", TxtPassword.Text);
+
+                        using (MySqlDataReader studentReader = studentCmd.ExecuteReader())
+                        {
+                            if (studentReader.Read())
+                            {
+                                MessageBox.Show("Login Success!");
+                                Form2 form2 = new Form2();
+                                this.Hide();
+                                form2.Show();
+                                return;
+                            }
+                        }
+                    }
+
+                    // FOR ADMIN
+                    /*using (MySqlCommand adminCmd = new MySqlCommand(adminQuery, connection))
+                    {
+                        adminCmd.Parameters.AddWithValue("@id", TxtStudentID.Text);
+                        adminCmd.Parameters.AddWithValue("@pass", TxtPassword.Text);
+
+                        using (MySqlDataReader adminReader = adminCmd.ExecuteReader())
+                        {
+                            if (adminReader.Read())
+                            {
+                                MessageBox.Show("Login Success! (Admin)");
+                                Admin_Home adminForm = new Admin_Home();
+                                this.Hide();
+                                adminForm.Show();
+                                return;
+                            }
+                        }
+                    }*/
+
+                    MessageBox.Show("Invalid credentials, please try again.");
+                    TxtStudentID.Clear();
+                    TxtPassword.Clear();
+                    ChkShowPass.Checked = false;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
         }
 
         private void BtnExit_Click(object sender, EventArgs e)
@@ -24,17 +88,7 @@ namespace HALAL2025_EVS
 
         private void BtnLogin_Click(object sender, EventArgs e)
         {
-            if(TxtStudentID.Text == "admin" && TxtPassword.Text == "root")
-            {
-                MessageBox.Show("Login Success", "Welcome", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                Form2 form2 = new Form2();
-                this.Hide();
-                form2.Show();
-            }
-            else
-            {
-                MessageBox.Show("Invalid credentials, please try again.");
-            }
+            Login();
         }
 
         private void ChkShowPass_CheckedChanged(object sender, EventArgs e)
