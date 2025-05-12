e8099e09adf37f0e7bbdb699ca2082bd04811a53
weimodein <jhorentuazon.dev@gmail.com>
2025-05-12 13:28:48 +0800

    CheckPlatform has been centered.

--- HALAL2025-EVS/CheckPlatform.Designer.cs
+++ HALAL2025-EVS/CheckPlatform.Designer.cs
@@ -197,6 +197,7 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "Platform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form12";
             this.PnlHeading.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.PicBoxLogo)).EndInit();
